4f3e053792c9a6dcfdb665dbdcb9f21eb153b3df
vigneshp25 <32658520+vigneshp25@users.noreply.github.com>
2024-04-17 21:56:34 +0530

    Component generated without cli

--- VigneshApp/src/app/add-to-cart/add-to-cart.component.ts
+++ VigneshApp/src/app/add-to-cart/add-to-cart.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CartCountComponent } from '../cart-count/cart-count.component';
 
 @Component({
   selector: 'app-add-to-cart',
   standalone: true,
-  imports: [],
+  imports: [CartCountComponent],
   templateUrl: './add-to-cart.component.html',
   styleUrl: './add-to-cart.component.scss'
 })
