1038cb4571a54ac8e238a7ca17000aa2ee4cc3e8
vigneshp25 <vigneshwaranp25@gmail.com>
2024-05-07 23:06:30 +0530

    changes

--- VigneshApp/src/app/cart-count/cart-count.component.ts
+++ VigneshApp/src/app/cart-count/cart-count.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
     selector: "app-cart-count",
     standalone: true,
+    imports: [CommonModule],
     templateUrl: "./cart-count.component.html",
     styleUrls: ["./cart-count.component.scss"]
 })
