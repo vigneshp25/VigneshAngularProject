6f0cfb265162f7e5dea2dc23722dbf4f0f03d66e
vigneshp25 <vigneshwaranp25@gmail.com>
2024-05-07 15:50:23 +0530

    Last week code changes

--- VigneshApp/src/app/cart-count/cart-count.component.ts
+++ VigneshApp/src/app/cart-count/cart-count.component.ts
@@ -1,33 +1,19 @@
 import { Component } from "@angular/core";
-import { ButtoncompComponent } from "../buttoncomp/buttoncomp.component";
 
 @Component({
     selector: "app-cart-count",
     standalone: true,
-    imports: [ButtoncompComponent],
     templateUrl: "./cart-count.component.html",
     styleUrls: ["./cart-count.component.scss"]
 })
-export class CartCountComponent{
+export class CartCountComponent {
     cartCount: number = 0;
     addition: string = "+";
     subtraction: string = "-";
-    isAddAllowed: boolean = false;
-    isSubAllowed: boolean = false;
-    addCartCount(){
-        if(this.cartCount < 5){
-            this.cartCount++;
-            this.isSubAllowed = false;
-        } else{
-            this.isAddAllowed = true;
-        }
+    addCartCount() {
+        this.cartCount++;
     }
-    removeCartCount(){
-        if(this.cartCount == 0){
-            this.isSubAllowed = true;
-        }else{
-            this.cartCount--;
-            this.isAddAllowed = false;
-        }
+    removeCartCount() {
+        this.cartCount--;
     }
 }
