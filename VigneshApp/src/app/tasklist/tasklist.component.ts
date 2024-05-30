d1a961acb47680ef729d72da9862d49bd063f5f4
Vigneshwaran <65214325+Vigneshwaranpalanivel25@users.noreply.github.com>
2024-05-30 10:46:29 +0530

    Ui changes

--- VigneshApp/src/app/tasklist/tasklist.component.ts
+++ VigneshApp/src/app/tasklist/tasklist.component.ts
@@ -1,15 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-tasklist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tasklist.component.html',
   styleUrl: './tasklist.component.scss'
 })
 export class TasklistComponent {
   taskListSearchText : string;
+  toDolist: Array<string> = ['task1', 'task2', 'task3'];
   constructor(){
     this.taskListSearchText = "";
+  }
+  addToToDOList(){
+    if(this.taskListSearchText.length > 0){
+      this.toDolist.push(this.taskListSearchText);
+      this.taskListSearchText = "";
+    }
   }
 }
