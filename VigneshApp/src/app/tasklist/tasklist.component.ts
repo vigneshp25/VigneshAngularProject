65bbf9ca092a57a9e77f475f303fb3d678b3e20e
Vigneshwaran <65214325+Vigneshwaranpalanivel25@users.noreply.github.com>
2024-06-02 00:00:23 +0530

    Switch case example
    Switch case example

--- VigneshApp/src/app/tasklist/tasklist.component.ts
+++ VigneshApp/src/app/tasklist/tasklist.component.ts
@@ -10,14 +10,18 @@ import { FormsModule } from '@angular/forms';
 })
 export class TasklistComponent {
   taskListSearchText : string;
-  toDolist: Array<string> = ['task1', 'task2', 'task3'];
+  toDoList = [{task: "Task 7", status: "ToDo"}, {task: "Task 8", status: "ToDo"}, {task: "Task 9", status: "ToDo"}];
+  inProgressList = [{task: "Task 4", status: "InProgress"}, {task: "Task 5", status: "InProgress"}, {task: "Task 6", status: "InProgress"}];
+  completedList = [{task: "Task 1", status: "Completed"}, {task: "Task 2", status: "Completed"}, {task: "Task 3", status: "Completed"}];
+  statusTOShow = "ToDo";
   constructor(){
     this.taskListSearchText = "";
   }
   addToToDOList(){
     if(this.taskListSearchText.length > 0){
-      this.toDolist.push(this.taskListSearchText);
+      this.toDoList.push({task: this.taskListSearchText, status: "ToDo"});
       this.taskListSearchText = "";
+      console.log(this.toDoList);
     }
   }
 }
