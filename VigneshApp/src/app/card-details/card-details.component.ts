67a99f3629445747e644892c473d3f1dee9dbac3
Vigneshwaran Palanivel <vigneshwaran.palanivel@bostonharborconsulting.com>
2024-07-03 17:30:40 +0530

    changes

--- VigneshApp/src/app/card-details/card-details.component.ts
+++ VigneshApp/src/app/card-details/card-details.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './card-details.component.html',
   styleUrl: './card-details.component.scss'
 })
 export class CardDetailsComponent {
-
+  userName: string = '';
+  cardNumber: number = 0;
+  amount: number = 0;
+  constructor(){
+    
+  }
 }
